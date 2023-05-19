a0c99ab7836a43cc3e08140b5597d9a11453a586
weealDCDEF <elynetobr@outlook.com>
2023-05-19 09:55:50 -0300

    siodksodoksoids

--- led_do_roque/led_do_roque.ino
+++ led_do_roque/led_do_roque.ino
@@ -1,6 +1,19 @@
 #include <LiquidCrystal.h>
 
 
+/*
+  LEFT:   481,478,480,481;
+  RIGHT:  0;
+  UP:     129,130, 131;
+  DOWN:   300,307;
+  SELECT: 722, 721, 774.
+*/
+
+
+/*Setting pin's*/
+
+
+const int PIN_ANALOG_READ = 0;
 const int PIN_RS = 8;
 const int PIN_EN = 9;
 const int PIN_D4 = 4;
@@ -8,10 +21,16 @@ const int PIN_D5 = 5;
 const int PIN_D6 = 6;
 const int PIN_D7 = 7;
 const int PIN_BL = 10;
-const int SRL_CFG = 9600;
 
+/*Setting constants*/
+const int SRL_CFG = 9600;
 const int SCREEN_X = 16;
 const int SCREEN_Y = 2;
+
+/*Setting const for if conditions:*/
+
+const int VAR_RIGHT = 100;
+
 
 LiquidCrystal lcd(PIN_RS,PIN_EN,PIN_D4,PIN_D5,PIN_D6,PIN_D7);
 
@@ -21,10 +40,34 @@ void setup() {
     lcd.setCursor(0,0);
     lcd.print("anthem");
     lcd.setCursor(0,1);
+    lcd.print("Tecla:");
 }
 
 void loop() {
-    static int x = analogRead(0);
-    Serial.println("x");
+    static int x = analogRead(PIN_ANALOG_READ);
+    Serial.println(x);
+    lcd.setCursor(7,2);
+
+    if  (x < 100)
+    {
+        lcd.println("RIGHT ");
+    }
+    else if (x < 200)
+    {
+        lcd.println("UP    ");
+    }
+    else if (x < 400)
+    {
+        lcd.println("DOWN  ");
+    }
+    else if (x < 600)
+    {
+        lcd.println("LEFT  ");  
+    }
+    else if (x < 800)
+    {
+      lcd.println("SELECT  ");  
+    }
+    
     delay(500);
 }
